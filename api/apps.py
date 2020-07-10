7c57d6cdb48829b48d17e3a48e40b4f211ced110
AHRojo <andres.huerta.14@sansano.usm.cl>
2020-07-10 12:22:25 -0400

    user

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,25 @@
 from django.apps import AppConfig
+from django.db import OperationalError
 
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        try:
+            username = os.environ['USER']
+            password = os.environ['PASSWORD']
+        except:
+            from .secrets import *
+
+        try:
+            user = User.objects.get(username=username)
+
+        except User.DoesNotExist:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+
+        except OperationalError:
+            pass
